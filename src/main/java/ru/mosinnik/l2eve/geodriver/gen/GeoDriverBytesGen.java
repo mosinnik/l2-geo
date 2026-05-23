9afb54f0e6cb9963adcbdbee3fbd091276212262
mosinnik <mosinnik@yandex.ru>
2026-05-23 03:16:22 +0300

    actualize Gen

--- src/main/java/ru/mosinnik/l2eve/geodriver/gen/GeoDriverBytesGen.java
+++ src/main/java/ru/mosinnik/l2eve/geodriver/gen/GeoDriverBytesGen.java
@@ -26,8 +26,9 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import ru.mosinnik.l2eve.geodriver.abstraction.IBlock;
 import ru.mosinnik.l2eve.geodriver.abstraction.IRegion;
-import ru.mosinnik.l2eve.geodriver.blocks.*;
+import ru.mosinnik.l2eve.geodriver.blocks.FlatBlock;
 import ru.mosinnik.l2eve.geodriver.bytes.*;
+import ru.mosinnik.l2eve.geodriver.driver.ByteUtil;
 import ru.mosinnik.l2eve.geodriver.driver.GeoConfig;
 import ru.mosinnik.l2eve.geodriver.regions.Region;
 
@@ -160,7 +161,7 @@ public final class GeoDriverBytesGen {
         for (RegionCoordinated regionCoordinated : regions) {
             Region region = regionCoordinated.region();
             for (int i = 0; i < IRegion.REGION_BLOCKS; i++) {
-                dataSize += getBytesCount(region.getBlock(i));
+                dataSize += ByteUtil.getBytesCount(region.getBlock(i));
                 totalBlockCount++;
             }
         }
@@ -183,7 +184,7 @@ public final class GeoDriverBytesGen {
             for (int i = 0; i < IRegion.REGION_BLOCKS; i++) {
                 IBlock block = region.getBlock(i);
 
-                byte blockType = getType(block);
+                byte blockType = ByteUtil.getType(block);
                 blockTypes[blockIndex] = blockType;
 
                 if (blockType == FLAT_BLOCK) {
@@ -191,7 +192,7 @@ public final class GeoDriverBytesGen {
                 } else {
                     int blockDataOffset = data.position();
                     blockDataOffsets[blockIndex] = blockDataOffset;
-                    appendBytes(block, data);
+                    ByteUtil.appendBytes(block, data);
                 }
 
                 blockIndex++;
@@ -225,123 +226,6 @@ public final class GeoDriverBytesGen {
         } catch (Exception e) {
             throw new RuntimeException("Failed to transform BaseDriver", e);
         }
-    }
-
-
-    public static byte getType(IBlock block) {
-        Class<? extends IBlock> blockClass = block.getClass();
-        if (blockClass.equals(FlatBlock.class)) {
-            return FLAT_BLOCK;
-        } else if (blockClass.equals(ComplexBlock.class)) {
-            return COMPLEX_BLOCK;
-        } else if (blockClass.equals(MultilayerBlock.class)) {
-            return MULTILAYER_BLOCK;
-        } else if (blockClass.equals(OneHeightComplexBlock.class)) {
-            return ONE_HEIGHT_COMPLEX_BLOCK;
-        } else if (blockClass.equals(BaseHeightComplexBlock.class)) {
-            return BASE_HEIGHT_COMPLEX_BLOCK;
-        } else if (blockClass.equals(BaseHeightOneNsweComplexBlock.class)) {
-            return BASE_HEIGHT_ONE_NSWE_COMPLEX_BLOCK;
-        } else if (blockClass.equals(FewHeightsComplexBlock.class)) {
-            return FEW_HEIGHTS_COMPLEX_BLOCK;
-        } else if (blockClass.equals(FewHeightsOneNsweComplexBlock.class)) {
-            return FEW_HEIGHTS_ONE_NSWE_COMPLEX_BLOCK;
-        } else if (blockClass.equals(NoHolesMultilayerBlock.class)) {
-            return NO_HOLES_MULTILAYER_BLOCK;
-        } else if (blockClass.equals(IndexedMultilayerBlock.class)) {
-            return INDEXED_MULTILAYER_BLOCK;
-        } else if (blockClass.equals(Indexed32MultilayerBlock.class)) {
-            return INDEXED_32_MULTILAYER_BLOCK;
-        }
-
-        throw new RuntimeException("Unknown block class: " + blockClass.getName());
-    }
-
-    public static byte[] toBytes(IBlock block) {
-        Class<? extends IBlock> blockClass = block.getClass();
-        if (blockClass.equals(FlatBlock.class)) {
-            return FlatBlockFromOffsetBytes.toBytes((FlatBlock) block);
-        } else if (blockClass.equals(ComplexBlock.class)) {
-            return ComplexBlockBytes.toBytes((ComplexBlock) block);
-        } else if (blockClass.equals(MultilayerBlock.class)) {
-            return MultilayerBlockBytes.toBytes((MultilayerBlock) block);
-        } else if (blockClass.equals(OneHeightComplexBlock.class)) {
-            return OneHeightComplexBlockBytes.toBytes((OneHeightComplexBlock) block);
-        } else if (blockClass.equals(BaseHeightComplexBlock.class)) {
-            return BaseHeightComplexBlockBytes.toBytes((BaseHeightComplexBlock) block);
-        } else if (blockClass.equals(BaseHeightOneNsweComplexBlock.class)) {
-            return BaseHeightOneNsweComplexBlockBytes.toBytes((BaseHeightOneNsweComplexBlock) block);
-        } else if (blockClass.equals(FewHeightsComplexBlock.class)) {
-            return FewHeightsComplexBlockBytes.toBytes((FewHeightsComplexBlock) block);
-        } else if (blockClass.equals(FewHeightsOneNsweComplexBlock.class)) {
-            return FewHeightsOneNsweComplexBlockBytes.toBytes((FewHeightsOneNsweComplexBlock) block);
-        } else if (blockClass.equals(NoHolesMultilayerBlock.class)) {
-            return NoHolesMultilayerBlockBytes.toBytes((NoHolesMultilayerBlock) block);
-        } else if (blockClass.equals(IndexedMultilayerBlock.class)) {
-            return IndexedMultilayerBlockBytes.toBytes((IndexedMultilayerBlock) block);
-        } else if (blockClass.equals(Indexed32MultilayerBlock.class)) {
-            return Indexed32MultilayerBlockBytes.toBytes((Indexed32MultilayerBlock) block);
-        }
-
-        throw new RuntimeException("Unknown block class: " + blockClass.getName());
-    }
-
-    public static void appendBytes(IBlock block, ByteBuffer data) {
-        Class<? extends IBlock> blockClass = block.getClass();
-        if (blockClass.equals(FlatBlock.class)) {
-            FlatBlockFromOffsetBytes.appendBytes((FlatBlock) block, data);
-        } else if (blockClass.equals(ComplexBlock.class)) {
-            ComplexBlockBytes.appendBytes((ComplexBlock) block, data);
-        } else if (blockClass.equals(MultilayerBlock.class)) {
-            MultilayerBlockBytes.appendBytes((MultilayerBlock) block, data);
-        } else if (blockClass.equals(OneHeightComplexBlock.class)) {
-            OneHeightComplexBlockBytes.appendBytes((OneHeightComplexBlock) block, data);
-        } else if (blockClass.equals(BaseHeightComplexBlock.class)) {
-            BaseHeightComplexBlockBytes.appendBytes((BaseHeightComplexBlock) block, data);
-        } else if (blockClass.equals(BaseHeightOneNsweComplexBlock.class)) {
-            BaseHeightOneNsweComplexBlockBytes.appendBytes((BaseHeightOneNsweComplexBlock) block, data);
-        } else if (blockClass.equals(FewHeightsComplexBlock.class)) {
-            FewHeightsComplexBlockBytes.appendBytes((FewHeightsComplexBlock) block, data);
-        } else if (blockClass.equals(FewHeightsOneNsweComplexBlock.class)) {
-            FewHeightsOneNsweComplexBlockBytes.appendBytes((FewHeightsOneNsweComplexBlock) block, data);
-        } else if (blockClass.equals(NoHolesMultilayerBlock.class)) {
-            NoHolesMultilayerBlockBytes.appendBytes((NoHolesMultilayerBlock) block, data);
-        } else if (blockClass.equals(IndexedMultilayerBlock.class)) {
-            IndexedMultilayerBlockBytes.appendBytes((IndexedMultilayerBlock) block, data);
-        } else if (blockClass.equals(Indexed32MultilayerBlock.class)) {
-            Indexed32MultilayerBlockBytes.appendBytes((Indexed32MultilayerBlock) block, data);
-        } else {
-            throw new RuntimeException("Unknown block class: " + blockClass.getName());
-        }
-    }
-
-    public static int getBytesCount(IBlock block) {
-        Class<? extends IBlock> blockClass = block.getClass();
-        if (blockClass.equals(FlatBlock.class)) {
-            return FlatBlockFromOffsetBytes.calcBytesCount((FlatBlock) block);
-        } else if (blockClass.equals(ComplexBlock.class)) {
-            return ComplexBlockBytes.calcBytesCount((ComplexBlock) block);
-        } else if (blockClass.equals(MultilayerBlock.class)) {
-            return MultilayerBlockBytes.calcBytesCount((MultilayerBlock) block);
-        } else if (blockClass.equals(OneHeightComplexBlock.class)) {
-            return OneHeightComplexBlockBytes.calcBytesCount((OneHeightComplexBlock) block);
-        } else if (blockClass.equals(BaseHeightComplexBlock.class)) {
-            return BaseHeightComplexBlockBytes.calcBytesCount((BaseHeightComplexBlock) block);
-        } else if (blockClass.equals(BaseHeightOneNsweComplexBlock.class)) {
-            return BaseHeightOneNsweComplexBlockBytes.calcBytesCount((BaseHeightOneNsweComplexBlock) block);
-        } else if (blockClass.equals(FewHeightsComplexBlock.class)) {
-            return FewHeightsComplexBlockBytes.calcBytesCount((FewHeightsComplexBlock) block);
-        } else if (blockClass.equals(FewHeightsOneNsweComplexBlock.class)) {
-            return FewHeightsOneNsweComplexBlockBytes.calcBytesCount((FewHeightsOneNsweComplexBlock) block);
-        } else if (blockClass.equals(NoHolesMultilayerBlock.class)) {
-            return NoHolesMultilayerBlockBytes.calcBytesCount((NoHolesMultilayerBlock) block);
-        } else if (blockClass.equals(IndexedMultilayerBlock.class)) {
-            return IndexedMultilayerBlockBytes.calcBytesCount((IndexedMultilayerBlock) block);
-        } else if (blockClass.equals(Indexed32MultilayerBlock.class)) {
-            return Indexed32MultilayerBlockBytes.calcBytesCount((Indexed32MultilayerBlock) block);
-        }
-
-        throw new RuntimeException("Unknown block class: " + blockClass.getName());
     }
 
     record RegionCoordinated(Region region, int regionX, int regionY) {
